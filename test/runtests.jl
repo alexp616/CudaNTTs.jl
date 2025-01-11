784a1d0a87834bcda0b132c0b6f15d2fa7fece03
alexp616 <alpan@ucsd.edu>
2025-01-11 00:39:37 -0800

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,85 @@
 using NTTs
 using Test
+using CUDA
+
+function test_correct()
+    p = UInt32(167772161)
+    for log2len in 1:4:21
+        len = 1 << log2len
+        npru = primitive_nth_root_of_unity(len, p)
+        cpuarr = rand(zero(typeof(p)):(p - one(p)), len)
+        gpuarr = CuArray(cpuarr)
+
+        nttplan, inttplan = plan_ntt(len, p, npru)
+        NTTs.cpu_ntt!(cpuarr, nttplan)
+        ntt!(gpuarr, nttplan)
+
+        @test cpuarr == Array(gpuarr)
+
+        NTTs.cpu_intt!(cpuarr, inttplan)
+        intt!(gpuarr, inttplan)
+
+        @test cpuarr == Array(gpuarr)
+    end
+
+    p = UInt64(167772161)
+    for log2len in 1:4:21
+        len = 1 << log2len
+        npru = primitive_nth_root_of_unity(len, p)
+        cpuarr = rand(zero(typeof(p)):(p - one(p)), len)
+        gpuarr = CuArray(cpuarr)
+
+        nttplan, inttplan = plan_ntt(len, p, npru)
+        NTTs.cpu_ntt!(cpuarr, nttplan)
+        ntt!(gpuarr, nttplan)
+
+        @test cpuarr == Array(gpuarr)
+
+        NTTs.cpu_intt!(cpuarr, inttplan)
+        intt!(gpuarr, inttplan)
+
+        @test cpuarr == Array(gpuarr)
+    end
+
+    p = Int32(167772161)
+    for log2len in 1:4:21
+        len = 1 << log2len
+        npru = primitive_nth_root_of_unity(len, p)
+        cpuarr = rand(zero(typeof(p)):(p - one(p)), len)
+        gpuarr = CuArray(cpuarr)
+
+        nttplan, inttplan = plan_ntt(len, p, npru)
+        NTTs.cpu_ntt!(cpuarr, nttplan)
+        ntt!(gpuarr, nttplan)
+
+        @test cpuarr == Array(gpuarr)
+
+        NTTs.cpu_intt!(cpuarr, inttplan)
+        intt!(gpuarr, inttplan)
+
+        @test cpuarr == Array(gpuarr)
+    end
+
+    p = Int64(167772161)
+    for log2len in 1:4:21
+        len = 1 << log2len
+        npru = primitive_nth_root_of_unity(len, p)
+        cpuarr = rand(zero(typeof(p)):(p - one(p)), len)
+        gpuarr = CuArray(cpuarr)
+
+        nttplan, inttplan = plan_ntt(len, p, npru)
+        NTTs.cpu_ntt!(cpuarr, nttplan)
+        ntt!(gpuarr, nttplan)
+
+        @test cpuarr == Array(gpuarr)
+
+        NTTs.cpu_intt!(cpuarr, inttplan)
+        intt!(gpuarr, inttplan)
+
+        @test cpuarr == Array(gpuarr)
+    end
+end
 
 @testset "NTTs.jl" begin
-    # Write your tests here.
+    test_correct()
 end
