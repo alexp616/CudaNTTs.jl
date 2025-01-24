4af760605dd3a94f779a806c426a286e37fb5888
alexp616 <alpan@ucsd.edu>
2025-01-23 16:44:54 -0800

    removed functionality for signed integers (who even uses these)

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,45 +22,7 @@ function test_correct()
         @test cpuarr == Array(gpuarr)
     end
 
-    p = UInt64(167772161)
-    for log2len in 1:4:21
-        len = 1 << log2len
-        npru = primitive_nth_root_of_unity(len, p)
-        cpuarr = rand(zero(typeof(p)):(p - one(p)), len)
-        gpuarr = CuArray(cpuarr)
-
-        nttplan, inttplan = plan_ntt(len, p, npru)
-        NTTs.cpu_ntt!(cpuarr, nttplan)
-        ntt!(gpuarr, nttplan)
-
-        @test cpuarr == Array(gpuarr)
-
-        NTTs.cpu_intt!(cpuarr, inttplan)
-        intt!(gpuarr, inttplan)
-
-        @test cpuarr == Array(gpuarr)
-    end
-
-    p = Int32(167772161)
-    for log2len in 1:4:21
-        len = 1 << log2len
-        npru = primitive_nth_root_of_unity(len, p)
-        cpuarr = rand(zero(typeof(p)):(p - one(p)), len)
-        gpuarr = CuArray(cpuarr)
-
-        nttplan, inttplan = plan_ntt(len, p, npru)
-        NTTs.cpu_ntt!(cpuarr, nttplan)
-        ntt!(gpuarr, nttplan)
-
-        @test cpuarr == Array(gpuarr)
-
-        NTTs.cpu_intt!(cpuarr, inttplan)
-        intt!(gpuarr, inttplan)
-
-        @test cpuarr == Array(gpuarr)
-    end
-
-    p = Int64(167772161)
+    p = UInt64(0x7ffffffff9000001)
     for log2len in 1:4:21
         len = 1 << log2len
         npru = primitive_nth_root_of_unity(len, p)
